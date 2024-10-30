e586a6c1d344e58c8ad68ee9f5218394784398f3
Sami Munir <sami.munir2001@gmail.com>
2024-10-29 22:36:24 -0400

    implementing loop to build HOUSE_DATA[] for each CITY by calling function scrape_zameen()

--- Rutgers/CS105/Project/zameen_scraper.py
+++ Rutgers/CS105/Project/zameen_scraper.py
@@ -1,6 +1,12 @@
 import requests as REQS
 from bs4 import BeautifulSoup as BS4
 
+# CONSTANTS
+NUMBER_OF_PAGES_TO_SEARCH = 20
+
+def scrape_web():
+    pass
+
 if __name__ == '__main__':
     HOUSE_DATA = []
     
@@ -13,3 +19,11 @@ if __name__ == '__main__':
         {'id': 6, 'name': 'Murree'},
         {'id': 7, 'name': 'Peshawar'}
     ]
+    
+    for CITY in CITIES:
+        HOUSE_DATA.append(
+            {
+                'city': CITY.get('name'),
+                'info': scrape_web(f"{CITY.get('name')}-{CITY.get('id')}", NUMBER_OF_PAGES_TO_SEARCH)
+            }
+        )
