f793d14c193ce5c1acdc6b75f0f62469832eebb8
Sami Munir <sami.munir2001@gmail.com>
2024-10-29 22:30:56 -0400

    implementing zameen_scraper.py to web-scrape zameen.com website and generate .csv data file. Currently working on initial setup with list of different cities

--- Rutgers/CS105/Project/zameen_scraper.py
+++ Rutgers/CS105/Project/zameen_scraper.py
@@ -1,5 +1,15 @@
 import requests as REQS
 from bs4 import BeautifulSoup as BS4
 
-if __name__ == 'main':
-    HOUSE_INFO = []
+if __name__ == '__main__':
+    HOUSE_DATA = []
+    
+    CITIES = [
+        {'id': 1, 'name': 'Gujranwala'},
+        {'id': 2, 'name': 'Islamabad'},
+        {'id': 3, 'name': 'Karachi'},
+        {'id': 4, 'name': 'Lahore'},
+        {'id': 5, 'name': 'Multan'},
+        {'id': 6, 'name': 'Murree'},
+        {'id': 7, 'name': 'Peshawar'}
+    ]
